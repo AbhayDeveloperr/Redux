e6f3578c7aba23200052cb2516d9203a0c238381
guruabhay25 <guptaabhay2727@gmail.com>
2026-06-20 13:30:19 +0530

    initial of redux

---
+++ tutorial/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const App = () => {
+
+
+  return (
+    <div>
+      <h1>0</h1>
+      <button onClick={()=>{
+        
+      }}>Increament</button>
+      <button onClick={()=>{
+
+      }}>Decreament</button>
+    </div>
+  )
+}
+
+export default App
